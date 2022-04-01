74ca7f4f7633bb28b169ec313d46b993008654c5
Christian Wang <christian.gnaw@gmail.com>
2022-04-01 17:55:07 -0400

    Added null time sig fix

--- src/index.js
+++ src/index.js
@@ -158,6 +158,8 @@ async function main() {
 
     for (let i = 0; i < main_time_signatures.length; i++) {
         let signatures = main_time_signatures[i]
+        if (!signatures.length)
+            continue
         for (let j = 0; j < main_time_signatures[i].length; j++) {
             let current_signature = signatures[j]
             //console.log(current_signature)
