500fec8599b208272a2207299b0a5f9252223bc6
Christian Wang <christian.gnaw@gmail.com>
2022-04-06 23:34:34 -0400

    WIP

--- src/insert.js
+++ src/insert.js
@@ -10,8 +10,10 @@ const LOOP_ALS = path.join(`${__dirname}`, "../", "/templates/sample_loop_projec
 // songs to pass in (input_filename, input_filename, input_filename)
 //let input_als = [LOOP_ALS, LOOP_ALS, "C:/Users/Chris/Desktop/projects/ableton_set_creator/spirit_of_the_living_god.als", "C:/Users/Chris/Desktop/projects/ableton_set_creator/build_my_life_d.als",]
 let input_als = [
+    "C:/Users/Chris/Desktop/worship_multitracks/Spirit Of The Living God-D-67bpm Project/Spirit Of The Living God-D-67bpm.als",
+    //"C:/Users/Chris/Desktop/worship_multitracks/Build My Life - D - 72bpm Project/Build My Life - D - 72bpm.als"
     //"C:/Users/chris/Desktop/3-27-2022/3-27-2022 Project/3-27-2022.als"
-    "C:/Users/Chris/Desktop/projects/ableton_set_creator/CS-Holy-Is-The-Lord-D-84.00bpm.als",
+    //"C:/Users/Chris/Desktop/projects/ableton_set_creator/CS-Holy-Is-The-Lord-D-84.00bpm.als",
     //"C:/Users/Chris/Desktop/projects/ableton_set_creator/CS-King-of-Kings-D.als",
     //"C:/Users/Chris/Desktop/projects/ableton_set_creator/Come-to-the-altar-D+.als",
     //"C:/Users/Chris/Desktop/projects/ableton_set_creator/CS-Desert-Song-D-110.00bpm.als"
@@ -31,7 +33,7 @@ let output_als = "mix.als"
 // output template
 let output_template = path.join(`${__dirname}`, "../", "template.als")//"/templates/main_template_project/template.als")
 
-input_als.push(output_template)
+//input_als.push(output_template)
 
 // check exists
 /*
@@ -144,39 +146,16 @@ async function main() {
         main_ends.push(end)
 
         console.log(taken_tracks_id)
-
-        if (midi_tracks) {
-            if (midi_tracks.length) {
-                for (let t = 0; t < midi_tracks.length; t++) {
-                    if (taken_tracks_id[midi_tracks[t]["$"]["Id"]]) {
-                        midi_tracks[t]["$"]["Id"] = Math.max(Number(Object.keys(taken_tracks_id))) + 1
-                        taken_tracks_id[midi_tracks[t]["$"]["Id"]] = 1
-                        //taken_tracks++
-                    } else {
-                        taken_tracks_id[midi_tracks[t]["$"]["Id"]] = 1
-                    }
-                }
-
-                main_midi_tracks = main_midi_tracks.concat(midi_tracks)
-            } else {
-                if (taken_tracks_id[midi_tracks["$"]["Id"]]) {
-                    midi_tracks["$"]["Id"] = Math.max(Number(Object.keys(taken_tracks_id))) + 1
-                    ///taken_tracks = Math.max(Number(Object.keys(taken_tracks_id))) 
-                    taken_tracks_id[midi_tracks["$"]["Id"]] = 1
-                } else {
-                    taken_tracks_id[midi_tracks["$"]["Id"]] = 1
-                }
-            }
-            main_midi_tracks.push(midi_tracks)
-        }
-
-       // console.log(tracks)
-
+        
+        
         if (return_tracks) {
             if (return_tracks.length) {
                 for (let t = 0; t < return_tracks.length; t++) {
+                    if (!return_tracks[t]["$"]["Id"]) {
+                        return_tracks[t]["$"]["Id"] = 0
+                    }
                     if (taken_tracks_id[return_tracks[t]["$"]["Id"]]) {
-                        return_tracks[t]["$"]["Id"] = Math.max(Number(Object.keys(taken_tracks_id))) + 1
+                        return_tracks[t]["$"]["Id"] = Math.max(Number(...Object.keys(taken_tracks_id))) + 1
                         taken_tracks_id[return_tracks[t]["$"]["Id"]] = 1
                         //taken_tracks++
                     } else {
@@ -185,24 +164,55 @@ async function main() {
                 }
                 main_return_tracks = main_return_tracks.concat(return_tracks)
             } else {
-                if (taken_tracks_id[main_return_tracks["$"]["Id"]]) {
-                    main_return_tracks["$"]["Id"] = Math.max(Number(Object.keys(taken_tracks_id))) + 1
+                if (!return_tracks["$"]["Id"]) {
+                    return_tracks["$"]["Id"] = 0
+                }
+                if (taken_tracks_id[return_tracks["$"]["Id"]]) {
+                    return_tracks["$"]["Id"] = Math.max(Number(...Object.keys(taken_tracks_id))) + 1
                     taken_tracks_id[return_tracks["$"]["Id"]] = 1
                     
                     //taken_tracks++
                 } else {
-                    taken_tracks_id[main_return_tracks["$"]["Id"]] = 1
+                    taken_tracks_id[return_tracks["$"]["Id"]] = 1
                 }
                 main_return_tracks.push(return_tracks)
             }
         }
+        
+        if (midi_tracks) {
+            if (midi_tracks.length) {
+                for (let t = 0; t < midi_tracks.length; t++) {
+                    if (taken_tracks_id[midi_tracks[t]["$"]["Id"]]) {
+                        midi_tracks[t]["$"]["Id"] = Math.max(Number(...Object.keys(taken_tracks_id))) + 1
+                        taken_tracks_id[midi_tracks[t]["$"]["Id"]] = 1
+                        //taken_tracks++
+                    } else {
+                        taken_tracks_id[midi_tracks[t]["$"]["Id"]] = 1
+                    }
+                    main_midi_tracks.push(midi_tracks[t])
+                }
+            } else {
+                console.log("NO")
+                if (taken_tracks_id[midi_tracks["$"]["Id"]]) {
+                    midi_tracks["$"]["Id"] = Math.max(Number(...Object.keys(taken_tracks_id))) + 1
+                    ///taken_tracks = Math.max(Number(...Object.keys(taken_tracks_id))) 
+                    taken_tracks_id[midi_tracks["$"]["Id"]] = 1
+                } else {
+                    taken_tracks_id[midi_tracks["$"]["Id"]] = 1
+                }
+            }
+            main_midi_tracks.push(midi_tracks)
+        }
+        
+       // consle.log(tracks
 
+        
         if (tracks) {
             if (tracks.length) {
                 for (let t = 0; t < tracks.length; t++) {
                     console.log(tracks[t]["$"]["Id"])
                     if (taken_tracks_id[tracks[t]["$"]["Id"]]) {
-                        tracks[t]["$"]["Id"] = Math.max(Number(Object.keys(taken_tracks_id))) + 1
+                        tracks[t]["$"]["Id"] = Math.max(Number(...Object.keys(taken_tracks_id))) + 1
                         taken_tracks_id[tracks[t]["$"]["Id"]] = 1
                         //taken_tracks++
                     } else {
@@ -213,10 +223,12 @@ async function main() {
                     if (clips) {
                         if (clips.length) {
                             for (let c = 0; c < clips.length; c++) {
+                                tracks[t]["DeviceChain"]["MainSequencer"]["Sample"]["ArrangerAutomation"]["Events"]["AudioClip"][c]["$"]["Time"] += t_locators[i]
                                 tracks[t]["DeviceChain"]["MainSequencer"]["Sample"]["ArrangerAutomation"]["Events"]["AudioClip"][c]["$"]["Id"] = taken_clips
                                 taken_clips++
                             }
                         } else {
+                            tracks[t]["DeviceChain"]["MainSequencer"]["Sample"]["ArrangerAutomation"]["Events"]["AudioClip"]["$"]["Time"] += t_locators[i]
                             tracks[t]["DeviceChain"]["MainSequencer"]["Sample"]["ArrangerAutomation"]["Events"]["AudioClip"]["$"]["Id"] = taken_clips
                             taken_clips++
                         }
@@ -226,7 +238,7 @@ async function main() {
             } else {
                 console.log(tracks["$"]["Id"])
                 if (taken_tracks_id[tracks["$"]["Id"]]) {
-                    tracks["$"]["Id"] = Math.max(Number(Object.keys(taken_tracks_id))) + 1
+                    tracks["$"]["Id"] = Math.max(Number(...Object.keys(taken_tracks_id))) + 1
                     taken_tracks_id[tracks["$"]["Id"]] = 1
                     //taken_tracks++
                 } else {
@@ -237,10 +249,12 @@ async function main() {
                 if (clips) {
                     if (clips.length) {
                         for (let c = 0; c < clips.length; c++) {
+                            tracks["DeviceChain"]["MainSequencer"]["Sample"]["ArrangerAutomation"]["Events"]["AudioClip"][c]["$"]["Time"] += t_locators[i]
                             tracks["DeviceChain"]["MainSequencer"]["Sample"]["ArrangerAutomation"]["Events"]["AudioClip"][c]["$"]["Id"] = taken_clips
                             taken_clips++
                         }
                     } else {
+                        tracks["DeviceChain"]["MainSequencer"]["Sample"]["ArrangerAutomation"]["Events"]["AudioClip"]["$"]["Time"] += t_locators[i]
                         tracks["DeviceChain"]["MainSequencer"]["Sample"]["ArrangerAutomation"]["Events"]["AudioClip"]["$"]["Id"] = taken_clips
                         taken_clips++
                     }
@@ -249,7 +263,7 @@ async function main() {
             }
         }
     }
-    console.log(taken_tracks_id)
+    //console.log(taken_tracks_id)
 
     //console.log(main_clips)
     // exit()
@@ -367,7 +381,7 @@ async function main() {
 
     //console.log(xml_obj["Ableton"]["LiveSet"]["Locators"]["Locators"])
 
-    // xml_obj["Ableton"]["LiveSet"]["Locators"]["Locators"]["Locator"] = final_locators
+    xml_obj["Ableton"]["LiveSet"]["Locators"]["Locators"]["Locator"] = final_locators
 
     if (!xml_obj["Ableton"]["LiveSet"]["MasterTrack"]["AutomationEnvelopes"]["Envelopes"]) {
         xml_obj["Ableton"]["LiveSet"]["MasterTrack"]["AutomationEnvelopes"]["Envelopes"] = {
@@ -394,19 +408,30 @@ async function main() {
     //console.log(main_tracks)
 
     xml_obj["Ableton"]["LiveSet"]["Tracks"]["AudioTrack"] = main_tracks
-    //xml_obj["Ableton"]["LiveSet"]["Tracks"]["MidiTrack"] = main_midi_tracks
+    xml_obj["Ableton"]["LiveSet"]["Tracks"]["MidiTrack"] = main_midi_tracks.slice(0,2)
 
-    //console.log(main_midi_tracks)
-    xml_obj["Ableton"]["LiveSet"]["Tracks"]["ReturnTrack"] = main_return_tracks
+    console.log(main_midi_tracks)
+    xml_obj["Ableton"]["LiveSet"]["Tracks"]["ReturnTrack"] = main_return_tracks.slice(0,2)
     //main_tracks = xml_obj["Ableton"]["LiveSet"]["Tracks"]["AudioTrack"] 
 
     //    fs.writeFileSync("test.json", JSON.stringify({ "Root": main_tracks }))
-    /*
-     if (!xml_obj["Ableton"]["LiveSet"]["MasterTrack"]["AutomationEnvelopes"]["Envelopes"]["AutomationEnvelope"][0]["Automation"]["Events"]["EnumEvent"].length) {
-         xml_obj["Ableton"]["LiveSet"]["MasterTrack"]["AutomationEnvelopes"]["Envelopes"]["AutomationEnvelope"][0]["Automation"]["Events"]["EnumEvent"] = xml_obj["Ableton"]["LiveSet"]["MasterTrack"]["AutomationEnvelopes"]["Envelopes"]["AutomationEnvelope"][0]["Automation"]["Events"]["EnumEvent"] = [xml_obj["Ableton"]["LiveSet"]["MasterTrack"]["AutomationEnvelopes"]["Envelopes"]["AutomationEnvelope"][0]["Automation"]["Events"]["EnumEvent"] = xml_obj["Ableton"]["LiveSet"]["MasterTrack"]["AutomationEnvelopes"]["Envelopes"]["AutomationEnvelope"][0]["Automation"]["Events"]["EnumEvent"]]
-     }
-     xml_obj["Ableton"]["LiveSet"]["MasterTrack"]["AutomationEnvelopes"]["Envelopes"]["AutomationEnvelope"][0]["Automation"]["Events"]["EnumEvent"] = xml_obj["Ableton"]["LiveSet"]["MasterTrack"]["AutomationEnvelopes"]["Envelopes"]["AutomationEnvelope"][0]["Automation"]["Events"]["EnumEvent"].concat(final_time_signatures)
-     */
+    if (xml_obj["Ableton"]["LiveSet"]["MasterTrack"]["AutomationEnvelopes"]["Envelopes"]["AutomationEnvelope"][0]["Automation"]["Events"]["EnumEvent"].length) {
+        let arr = xml_obj["Ableton"]["LiveSet"]["MasterTrack"]["AutomationEnvelopes"]["Envelopes"]["AutomationEnvelope"][0]["Automation"]["Events"]["EnumEvent"]
+
+        for (let a = 0; a < arr.length; a++) {
+            arr[a]["$"]["Id"] = largest_tim_id + 1
+            largest_tim_id++
+        }
+
+        xml_obj["Ableton"]["LiveSet"]["MasterTrack"]["AutomationEnvelopes"]["Envelopes"]["AutomationEnvelope"][0]["Automation"]["Events"]["EnumEvent"] = arr.concat(final_time_signatures)
+    } else {
+        let arr = xml_obj["Ableton"]["LiveSet"]["MasterTrack"]["AutomationEnvelopes"]["Envelopes"]["AutomationEnvelope"][0]["Automation"]["Events"]["EnumEvent"]
+        arr["$"]["Id"] = largest_tim_id + 1
+        largest_tim_id++
+        xml_obj["Ableton"]["LiveSet"]["MasterTrack"]["AutomationEnvelopes"]["Envelopes"]["AutomationEnvelope"][0]["Automation"]["Events"]["EnumEvent"] = [arr].concat(final_time_signatures)
+
+    }
+    
     xml_obj["Ableton"]["LiveSet"]["NextPointeeId"]["$"]["Value"] = 30000
 
     let xml = new xml2js.Builder({ headless: false, explicitArray: false, mergeAttrs: false, explicitCharkey: true }).buildObject(xml_obj)
